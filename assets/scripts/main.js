58f35d4e8ae7cb76cdaf4b4de2d853955ab14e2d
faquanly <78623809+faquanly@users.noreply.github.com>
2024-05-19 19:31:25 -0700

    updated main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -72,7 +72,7 @@ function initFormHandler() {
 	const formElelement = document.querySelector('form');
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	form.addEventListener('submit', function(event) {
+	formElelement.addEventListener('submit', function(event) {
 		event.preventDefault();
 		 	// Steps B4-B9 will occur inside the event listener from step B3
 	// B4. TODO - Create a new FormData object from the <form> element reference above
